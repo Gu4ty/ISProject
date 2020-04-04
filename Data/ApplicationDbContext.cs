23b1be60c15820c6d8b6810bece65f8651e22b9f
gu4ty <guatydavid@gmail.com>
2020-04-03 20:52:15 -0400

    Add Seeding Data
    Añadidos los siguientes modelos: -Product -User -Seller - ProductSale

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ISProject.Models;
 using Microsoft.AspNetCore.Identity;
+using ISProject.Utils;
 
 namespace ISProject.Data
 {
@@ -22,6 +23,101 @@ namespace ISProject.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            
+        }
+        protected override void OnModelCreating (ModelBuilder modelBuilder){
+            base.OnModelCreating(modelBuilder);
+
+            for(int i=1;i<=10;i++){
+                //Seeding 10 products...
+                modelBuilder.Entity<Product>()
+                    .HasData(new Product(){Id=i*10,Name="Producto" + i.ToString(), 
+                             Description="Descripcion del producto" + i.ToString()    });
+
+                //Seeding 10 users(customers)
+                RegisterUser(modelBuilder,(i*100).ToString(),SD.CustomerUser,"Customer"+i.ToString() );
+
+                //Seeding 10 seller
+                RegisterSeller(modelBuilder, (i*1000).ToString(), SD.SellerUser, "Seller" +i.ToString() );
+
+                //Seeding 10 products sales
+                modelBuilder.Entity<ProductSale>()
+                    .HasData(new ProductSale()
+                        {Id= i *1000 + 3, 
+                        ProductId= i*10,
+                        SellerId=(i*1000).ToString(),
+                        Units= i,
+                        Price = i/2,
+                    });
+            }
+
+               
+
+
+        }
+        private void RegisterUser(ModelBuilder modelBuilder,string id, string role, string name )
+        {
+            string User_ID = id;
+            string ROLE_ID = role;
+            
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = ROLE_ID,
+                Name = "admin",
+                NormalizedName = "admin"
+            });
+            
+            var hasher = new PasswordHasher<User>();
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = User_ID,
+                Name = name,
+                UserName = name,
+                NormalizedUserName = name,
+                Email = name + "@fake.com",
+                NormalizedEmail = name + "@fake.com",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "pass"),
+                SecurityStamp = string.Empty
+            });
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = ROLE_ID,
+                UserId = User_ID
+            }); 
+
+        }
+        private void RegisterSeller(ModelBuilder modelBuilder,string id, string role, string name )
+        {
+            string Seller_ID = id;
+            string ROLE_ID = role;
+            
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = ROLE_ID,
+                Name = "admin",
+                NormalizedName = "admin"
+            });
+            
+            var hasher = new PasswordHasher<Seller>();
+            modelBuilder.Entity<Seller>().HasData(new Seller
+            {
+                Id = Seller_ID,
+                Name = name,
+                UserName = name,
+                NormalizedUserName = name,
+                Email = name + "@fake.com",
+                NormalizedEmail = name + "@fake.com",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "pass"),
+                SecurityStamp = string.Empty
+            });
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = ROLE_ID,
+                UserId = Seller_ID
+            }); 
+
         }
     }
 }
