4291043ea2d3a2b3774df38d2d5879350cef3954
Carmen I Cabrera <kr1000a@gmail.com>
2020-04-08 22:16:27 -0400

    refs #6 - Shopping Cart option number changes dynamically
    When a product is added to the Shopping Cart, the number of different products displayed in the Shopping Cart sign in the navigation bar changes automatically

--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -28,6 +28,15 @@ namespace ISProject.Controllers
 
         public async Task<IActionResult> Index()
         {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if(claim != null)
+            {
+                var cnt = _db.ShoppingCart.Where(u => u.UserId == claim.Value).ToList().Count;
+                HttpContext.Session.SetInt32(SD.ssShoppingCartCount, cnt);
+            }
+
             return View(await _db.ProductSale.Include(s => s.Product).Include(s => s.Seller).ToListAsync());
         }
 
@@ -73,7 +82,7 @@ namespace ISProject.Controllers
 
 
                 var count = _db.ShoppingCart.Where(c => c.UserId == shCart.UserId).ToList().Count();
-                HttpContext.Session.SetInt32("ssCart", count);
+                HttpContext.Session.SetInt32(SD.ssShoppingCartCount, count);
 
                 return RedirectToAction("Index");
             }
