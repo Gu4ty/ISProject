a61d2aa6aa8e1f67ba74f39e0151303dbfa7875c
gu4ty <guatydavid@gmail.com>
2020-04-09 23:28:44 -0400

    [NotiAPI] ref #18 - Adding the SendNotiRole method
    SendNotiRole(db,userID) sends to all admins a noti of Role Change

--- Utils/NotiAPI.cs
+++ Utils/NotiAPI.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ISProject.Data;
 using ISProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ISProject.Utils
 {
@@ -8,6 +12,30 @@ namespace ISProject.Utils
         public static void SetSeen(Notification n){
             n.Seen=true;  
            
+        }
+
+        public static async Task<bool> SendNotiRole(ApplicationDbContext db,string UserId, DateTime? date = null)
+        {
+            if(date ==null)
+                date = DateTime.Now;
+            
+
+            var user = await db.User.FirstOrDefaultAsync(u => u.Id==UserId);
+            if(user == null)
+                return false;
+
+            var noti_role = new NotiRole(){
+                Message = "The Customer " + user.Name + " wants to become in to a seller",
+                NotiDate = (DateTime)date,
+                Seen = false,
+                UserID = UserId,
+                User = user,
+                SendToUser = "All_A",
+            };
+            await db.NotiRole.AddAsync(noti_role);
+            await db.SaveChangesAsync();
+            
+            return true;
         }
     }
 }
