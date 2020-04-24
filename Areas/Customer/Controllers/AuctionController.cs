dcf960a0ac74fb7e72614990a2a3edef6e5212aa
Carmen I Cabrera <kr1000a@gmail.com>
2020-04-24 17:54:54 -0400

    refs #49 - Select products for auction view
    When a seller presses Create Auction in My Products View, he is redirected to a page where he can decide the amount he is going to take from the products he is selling, to make the new auction.

--- Areas/Customer/Controllers/AuctionController.cs
+++ Areas/Customer/Controllers/AuctionController.cs
@@ -27,9 +27,45 @@ namespace ISProject.Areas.Customer.Controllers
         [Authorize(Roles=SD.SellerUser)]
         public async Task<IActionResult> Select()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var products = await _db.ProductSale.Include(p => p.Product)
+                                    .Where(p => p.SellerId == claim.Value && p.Units > 0)
+                                    .ToListAsync();
+
+            List<ProductsAuctionViewModel> prodAuctionList = new List<ProductsAuctionViewModel>();
+
+            foreach(var prod in products){
+                var prodAuction = new ProductsAuctionViewModel(){
+                    ProductSale = prod,
+                    Quantity = 0
+                };
+                prodAuctionList.Add(prodAuction);
+            }
+
+            return View(prodAuctionList);  
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Select(List<ProductsAuctionViewModel> prodAuctionList)
+        {
+            var newProdList = new List<ProductsAuctionViewModel>();
+            foreach(var item in prodAuctionList){
+                if(item.Quantity > 0){
+                    if(item.Quantity > item.ProductSale.Units){
+                        item.Quantity = item.ProductSale.Units;
+                    }
+                    newProdList.Add(item);
+                }
+            }    
 
+            return RedirectToAction("Create", newProdList);
+        }
+
+        public async Task<IActionResult> Create(List<ProductsAuctionViewModel> prodAuctionList){
+            return View();
+        }
     }
 }
